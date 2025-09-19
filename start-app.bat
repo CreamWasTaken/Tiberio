f266dccedd5e80995dc104399a8444a614ea79de
CreamWasTaken <longswordx97@gmail.com>
2025-09-19 14:59:59 +0800

    added to hide cmd after starting and added .bat to stop System

--- start-app.bat
+++ start-app.bat
@@ -1,11 +1,11 @@
+
 @echo off
-echo Starting Tiberio Application...
-echo.
-echo This will start both the backend server and frontend development server.
-echo.
-cd /d "paste/your/project/path/here"
-echo Current directory: %CD%
-echo.
-echo Running npm run dev...
-npm run dev
-pause
+
+if not defined HIDDEN_WINDOW (
+    start "" /B wscript.exe "%~dp0start-hidden.vbs"
+    exit
+)
+
+:: Please replace the path with your path of the backend 
+cd /d "C:\Users\Jamin\Desktop\Tiberio\Backend"
+call npm run dev
