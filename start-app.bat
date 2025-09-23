0091b0fec025f430220e7db01cecac7912bc3cb0
mamawski01 <72120572+mamawski01@users.noreply.github.com>
2025-09-23 13:06:59 +0800

    client

--- start-app.bat
+++ start-app.bat
@@ -1,14 +1,3 @@
-`@echo off
-echo Starting Tiberio Application...
-echo.
-echo This will start both the backend server and frontend development server.
-echo.
-cd /d "C:\Users\Administrator\Desktop\New System\Tiberio\Backend"
-echo Current directory: %CD%
-echo.
-echo Running npm run dev...
-npm run dev
-pause
 
 @echo off
 
